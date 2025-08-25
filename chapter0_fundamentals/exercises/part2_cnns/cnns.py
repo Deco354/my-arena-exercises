6323e39e23b23eb9ccd83f236982fd29fb1153c0
Declan McKenna <deco354@gmail.com>
2025-08-25 17:12:54 +0100

    Make modules

--- chapter0_fundamentals/exercises/part2_cnns/cnns.py
+++ chapter0_fundamentals/exercises/part2_cnns/cnns.py
@@ -39,3 +39,183 @@ import part2_cnns.utils as utils
 from plotly_utils import line
 
 # %%
+device = t.device(
+    "mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu"
+)
+print(device)
+# %%
+class ReLU(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        return x.max(t.tensor(0.0))
+
+
+tests.test_relu(ReLU)
+# %%
+class Linear(nn.Module):
+    def __init__(self, in_features: int, out_features: int, bias=True):
+        """
+        A simple linear (technically, affine) transformation.
+
+        The fields should be named `weight` and `bias` for compatibility with PyTorch.
+        If `bias` is False, set `self.bias` to None.
+        """
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.bias = bias
+
+        kaiming_scale = in_features**-0.5
+        weight_T: Float[Tensor, "out_feat in_feat"] = t.rand(out_features, in_features) * kaiming_scale * 2 - 1
+        self.weight = nn.Parameter(weight_T)
+        if bias:
+            bias = t.rand(out_features) * kaiming_scale * 2 - 1
+            self.bias = nn.Parameter(bias)
+        else:
+            self.bias = None
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        x: shape (*, in_features)
+        Return: shape (*, out_features)
+        """
+        z = einops.einsum(self.weight, x, "out_feat in_feat, batch in_feat -> batch out_feat")
+        if self.bias is not None:
+            z += self.bias
+        return z
+
+    def extra_repr(self) -> str:
+        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
+
+
+tests.test_linear_parameters(Linear, bias=False)
+tests.test_linear_parameters(Linear, bias=True)
+tests.test_linear_forward(Linear, bias=False)
+tests.test_linear_forward(Linear, bias=True)
+# %%
+class Flatten(nn.Module):
+    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input: Tensor) -> Tensor:
+        """
+        Flatten out dimensions from start_dim to end_dim, inclusive of both.
+        """
+        shape = input.shape
+
+        # Get start & end dims, handling negative indexing for end dim
+        start_dim = self.start_dim
+        end_dim = self.end_dim if self.end_dim >= 0 else len(shape) + self.end_dim
+
+        # Get the shapes to the left / right of flattened dims, as well as size of flattened middle
+        shape_left = shape[:start_dim]
+        shape_right = shape[end_dim + 1 :]
+        shape_middle = t.prod(t.tensor(shape[start_dim : end_dim + 1])).item()
+
+        return t.reshape(input, shape_left + (shape_middle,) + shape_right)
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+    #%%
+
+class SimpleMLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = Flatten()
+        self.linear1 = Linear(in_features=28 * 28, out_features= 100)
+        self.relu = ReLU()
+        self.linear2 = Linear(in_features=100, out_features=10)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.linear2(self.relu((self.linear1(self.flatten(x)))))
+
+tests.test_mlp_module(SimpleMLP)
+tests.test_mlp_forward(SimpleMLP)
+
+# %%
+MNIST_TRANSFORM = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(0.1307, 0.3081),
+    ]
+)
+
+
+def get_mnist(trainset_size: int = 10_000, testset_size: int = 1_000) -> tuple[Subset, Subset]:
+    """Returns a subset of MNIST training data."""
+
+    # Get original datasets, which are downloaded to "./data" for future use
+    mnist_trainset = datasets.MNIST(
+        exercises_dir / "data", train=True, download=True, transform=MNIST_TRANSFORM
+    )
+    mnist_testset = datasets.MNIST(
+        exercises_dir / "data", train=False, download=True, transform=MNIST_TRANSFORM
+    )
+
+    # # Return a subset of the original datasets
+    mnist_trainset = Subset(mnist_trainset, indices=range(trainset_size))
+    mnist_testset = Subset(mnist_testset, indices=range(testset_size))
+
+    return mnist_trainset, mnist_testset
+
+
+mnist_trainset, mnist_testset = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+mnist_testloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+
+# Get the first batch of test data, by starting to iterate over `mnist_testloader`
+for img_batch, label_batch in mnist_testloader:
+    print(f"{img_batch.shape=}\n{label_batch.shape=}\n")
+    break
+
+# Get the first datapoint in the test set, by starting to iterate over `mnist_testset`
+for img, label in mnist_testset:
+    print(f"{img.shape=}\n{label=}\n")
+    break
+
+t.testing.assert_close(img, img_batch[0])
+assert label == label_batch[0].item()
+# %%
+
+model = SimpleMLP().to(device)
+
+batch_size = 128
+epochs = 3
+
+mnist_trainset, _ = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=batch_size, shuffle=True)
+
+optimizer = t.optim.Adam(model.parameters(), lr=1e-3)
+loss_list = []
+
+for epoch in range(epochs):
+    pbar = tqdm(mnist_trainloader)
+
+    for imgs, labels in pbar:
+        # Move data to device, perform forward pass
+        imgs, labels = imgs.to(device), labels.to(device)
+        logits = model(imgs)
+
+        # Calculate loss, perform backward pass
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # Update logs & progress bar
+        loss_list.append(loss.item())
+        pbar.set_postfix(epoch=f"{epoch + 1}/{epochs}", loss=f"{loss:.3f}")
+
+
+# %%
+line(
+    loss_list,
+    x_max=epochs * len(mnist_trainset),
+    labels={"x": "Examples seen", "y": "Cross entropy loss"},
+    title="SimpleMLP training on MNIST",
+    width=700,
+)
+
+
+# %%
