1a5e66bd8cd9fded1784dc1bdb1eeb4b767fd905
Declan McKenna <deco354@gmail.com>
2025-11-21 17:42:17 +0000

    Push code for first distributed training exercise
    I need multiple GPUs for this which my mac doesn't have
    so I'm pushing everything so I can move it to
    a vm

--- chapter0_fundamentals/exercises/part3_optimization/optimization.py
+++ chapter0_fundamentals/exercises/part3_optimization/optimization.py
@@ -645,3 +645,80 @@ wandb.agent(sweep_id=sweep_id, function=train, count=3)
 wandb.finish()
 
 # %%
+WORLD_SIZE = t.cuda.device_count()
+
+os.environ["MASTER_ADDR"] = "localhost"
+os.environ["MASTER_PORT"] = "12345"
+
+
+def send_receive(rank, world_size):
+    dist.init_process_group(backend="gloo", rank=rank, world_size=world_size)
+
+    if rank == 0:
+        # Send tensor to rank 1
+        sending_tensor = t.zeros(1)
+        print(f"{rank=}, sending {sending_tensor=}")
+        dist.send(tensor=sending_tensor, dst=1)
+    elif rank == 1:
+        # Receive tensor from rank 0
+        received_tensor = t.ones(1)
+        print(f"{rank=}, creating {received_tensor=}")
+        dist.recv(
+            received_tensor, src=0
+        )  # this line overwrites the tensor's data with our `sending_tensor`
+        print(f"{rank=}, received {received_tensor=}")
+
+    dist.destroy_process_group()
+
+
+if MAIN:
+    world_size = 2  # simulate 2 processes
+    mp.spawn(
+        send_receive,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True,
+    )
+
+# %%
+def send_receive_nccl(rank, world_size):
+    dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
+    device = t.device(f"cuda:{rank}")
+
+    if rank == 0:
+        # Create a tensor, send it to rank 1
+        sending_tensor = t.tensor([rank], device=device)
+        print(f"{rank=}, {device=}, sending {sending_tensor=}")
+        dist.send(sending_tensor, dst=1)  # Send tensor to CPU before sending
+    elif rank == 1:
+        # Receive tensor from rank 0 (it needs to be on the CPU before receiving)
+        received_tensor = t.tensor([rank], device=device)
+        print(f"{rank=}, {device=}, creating {received_tensor=}")
+        dist.recv(
+            received_tensor, src=0
+        )  # this line overwrites the tensor's data with our `sending_tensor`
+        print(f"{rank=}, {device=}, received {received_tensor=}")
+
+    dist.destroy_process_group()
+
+
+if MAIN:
+    world_size = 2  # simulate 2 processes
+    mp.spawn(
+        send_receive_nccl,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True,
+    )
+
+# %%
+def broadcast(tensor: Tensor, rank: int, world_size: int, src: int = 0):
+    """
+    Broadcast averaged gradients from rank 0 to all other ranks.
+    """
+    raise NotImplementedError()
+
+
+if MAIN:
+    tests.test_broadcast(broadcast, WORLD_SIZE)
