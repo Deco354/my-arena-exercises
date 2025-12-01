13dc4c74de31e1e7bd1c7e92cf23f9567cd6c550
Declan McKenna <deco354@gmail.com>
2025-12-01 10:17:37 +0100

    Remove duplicate AdamW implementation

--- chapter0_fundamentals/exercises/part3_optimization/optimization.py
+++ chapter0_fundamentals/exercises/part3_optimization/optimization.py
@@ -290,53 +290,6 @@ for optimizer_class, params in optimizer_list:
     points.append((xys, optimizer_class, params))
 
 plot_fn_with_points(pathological_curve_loss, min_points=[(0, "y_min")], points=points)
-
-# %%
-class AdamW:
-    def __init__(
-        self,
-        params: Iterable[t.nn.parameter.Parameter],
-        lr: float = 0.001,
-        betas: tuple[float, float] = (0.9, 0.999),
-        eps: float = 1e-08,
-        weight_decay: float = 0.0,
-    ):
-        """Implements Adam.
-
-        Like the PyTorch version, but assumes amsgrad=False and maximize=False
-            https://pytorch.org/docs/stable/generated/torch.optim.AdamW.html
-        """
-        self.params = list(params)
-        self.lr = lr
-        self.beta1, self.beta2 = betas
-        self.eps = eps
-        self.lmda = weight_decay
-        self.t = 1
-
-        self.m = [t.zeros_like(p) for p in self.params]
-        self.v = [t.zeros_like(p) for p in self.params]
-
-    def zero_grad(self) -> None:
-        for p in self.params:
-            p.grad = None
-
-    @t.inference_mode()
-    def step(self) -> None:
-        for theta, m, v in zip(self.params, self.m, self.v):
-            g = theta.grad
-            theta *= 1 - self.lr * self.lmda
-            m.copy_(self.beta1 * m + (1 - self.beta1) * g)
-            v.copy_(self.beta2 * v + (1 - self.beta2) * g.pow(2))
-            m_hat = m / (1 - self.beta1**self.t)
-            v_hat = v / (1 - self.beta2**self.t)
-            theta -= self.lr * m_hat / (v_hat.sqrt() + self.eps)
-        self.t += 1
-
-    def __repr__(self) -> str:
-        return (
-            f"AdamW(lr={self.lr}, beta1={self.beta1}, beta2={self.beta2}, eps={self.eps}, "
-            f"weight_decay={self.lmda})"
-        )
     
 # %%
 class AdamW:
